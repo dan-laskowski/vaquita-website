7a981c808b127ed4fc4dfa1273fd0d8e32e135b4
Daniel <daniel.laskowski94@gmail.com>
2021-06-06 21:11:39 +0200

    fix mailchimp endpoint

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       resolve: "gatsby-plugin-mailchimp",
       options: {
         endpoint:
-          "ttps://gmail.us10.list-manage.com/subscribe/post?u=fca57e710b56c2699e6e8f724&amp;id=2e7958e50b", // string; add your MC list endpoint here; see instructions below
+          "https://gmail.us10.list-manage.com/subscribe/post?u=fca57e710b56c2699e6e8f724&amp;id=2e7958e50b", // string; add your MC list endpoint here; see instructions below
         timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
     },
