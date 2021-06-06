6bc9e43b6e65774da15e27b952841d455684c2ae
Daniel <daniel.laskowski94@gmail.com>
2021-06-06 18:02:10 +0200

    ui fix for hero section

--- src/components/organisms/mainSection.js
+++ src/components/organisms/mainSection.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { GatsbyImage } from "gatsby-plugin-image";
 import CTAButton from "../atoms/button";
 import { Heading, Subheading } from "../atoms/headings";
+import Input from "../atoms/input";
 
 const StyledSection = styled.section`
   display: flex;
@@ -11,19 +12,19 @@ const StyledSection = styled.section`
   min-height: 100vh;
   .wrapper {
     display: flex;
-    width: 80%;
     align-items: center;
   }
   .image-wrapper,
   .text {
     width: 50%;
+    max-width: 600px;
     margin: 2rem;
     display: flex;
     flex-direction: column;
     justify-content: center;
     h1 {
-      font-size: 4.6rem;
-      line-height: 5.6rem;
+      font-size: 5.6rem;
+      line-height: 6rem;
     }
     h2 {
       font-size: 1.6rem;
@@ -35,6 +36,12 @@ const StyledSection = styled.section`
   }
   .image {
     width: 400px;
+  }
+  input {
+    max-width: 414px;
+    min-width: unset;
+    margin-top: 1rem;
+    margin-bottom: 0;
   }
 
   .buttons {
@@ -50,12 +57,17 @@ const MainSection = ({
   return (
     <StyledSection id={name} key={name} background={background}>
       <div className="wrapper">
-        <div className="image-wrapper">
-          <GatsbyImage className="image" image={image} alt="hero" />
-        </div>
         <div className="text">
           <Heading color={color}>{heading}</Heading>
           {paragraph && <Subheading color={color}>{paragraph}</Subheading>}
+          <Input
+            aria-label="email"
+            type="email"
+            placeholder="Enter your email"
+            maxLength="100"
+            name="reply_to"
+            required
+          />
           <div className="buttons">
             <CTAButton>Sign up to beta</CTAButton>
             <CTAButton primary>
@@ -63,6 +75,9 @@ const MainSection = ({
             </CTAButton>
           </div>
           {children}
+        </div>
+        <div className="image-wrapper">
+          <GatsbyImage className="image" image={image} alt="hero" />
         </div>
       </div>
     </StyledSection>
