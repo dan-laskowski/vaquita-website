33b10bdd2548a6b41e2f770ade2571d9338b0090
Daniel <daniel.laskowski94@gmail.com>
2021-06-07 11:54:57 +0200

    update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
